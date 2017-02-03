1e7d05ecd9a56d1719d1cb960aebf0b00b1efda1
Xonshiz <kanojia24.10@gmail.com>
2017-02-03 06:39:30 +0530

    PrioritScheduling

--- Operating Systems/priorityScheduling.py
+++ Operating Systems/priorityScheduling.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env Python
 from sys import exit
 '''
-Currently this module needs the FCFS implementation. Right now, it only sorts all the processes based on their
-priority. If the priority is same, then the program does the FCFS. It is unclear whether the program
-performs "FCFS" scheduling that we did earlier, or it just allocates whoever comes first.
-Right now, the program is allocating the resources on the basis of whoever is coming first. Nothing else
-has been implemented yet.
+Thanks to Dvlv (https://www.reddit.com/user/Dvlv) for his HUGE help.
+https://www.reddit.com/r/learnpython/comments/5rnd9s/sorting_multiple_lists_based_on_2_lists/dd8pc8k/
 '''
 def main():
     process_names = []
     burst_time = []
     priority = []
     timer = []
+    waiting_time = []
+    turn_around = []
     total_processes = int(input("Enter the total number of processes : "))
     if total_processes == 0:
         print("You need to enter at least 1 process to Schedule!")
@@ -23,30 +22,34 @@ def main():
             burst_time.append(int(input("Enter the burst time for the process : ")))
             priority.append(int(input("Enter the Priority of the process : ")))
 
-        timer, priority, process_names, burst_time = zip(*sorted(zip(timer, priority, process_names, burst_time)))
-        for a, b, c, d in zip(process_names,priority, timer, burst_time):
-            print(str(a) + ' \t\t\t\t|', str(b) + ' \t\t|', str(d) + ' \t\t|', str(c))
+        priority, timer, process_names, burst_time = zip(*sorted(zip(priority, timer, process_names, burst_time), reverse=True, key=lambda x: (x[0], -x[1])))
 
-        priority, timer, process_names, burst_time = zip(*sorted(zip(priority, timer, process_names, burst_time), reverse=True))
-        for a, b, c, d in zip(process_names,priority, timer, burst_time):
-            print(str(a) + ' \t\t\t\t|', str(b) + ' \t\t|', str(d) + ' \t\t|', str(c))
+        # print("Process Name\t| Priority\t| Burst Time")
+        # print('-' * 60)
+        #
+        # for a, b, c, d in zip(process_names,priority, timer, burst_time):
+        #     print(str(a) + ' \t\t\t\t|', str(b) + ' \t\t|', str(d))
 
-        print('\n')
-        print(priority)
-        print(burst_time)
-        # print(set([x for x in priority if priority.count(x) > 1]))
-        # dupes = set([x for x in priority if priority.count(x) > 1])
-        # dupes = list(i for i, x in enumerate(timer) if timer.count(x) > 1)
-        # print(dupes)
-        # for a in dupes:
-        #     print(a)
-        #     timer, priority, process_names, burst_time = zip(*sorted(zip(timer[a], priority[a], process_names[a], burst_time[a])))
+        for i in range(int(total_processes)):
+            if i == 0:
+                waiting_time.insert(0, 0)
+            waitingCalculator = int(burst_time[i]) + int(waiting_time[i])
+            waiting_time.insert(i + 1, waitingCalculator)
 
-        print("Process Name\t| Priority\t| Burst Time")
+            turnaroundCalculator = int(burst_time[i]) + int(waiting_time[i])
+            turn_around.insert(i, turnaroundCalculator)
+
+        waiting_time.pop()
+        print("Process Name\t| Burst Time\t| Waiting Time\t| Turn Around Time")
         print('-' * 60)
 
-        for a, b, c, d in zip(process_names,priority, timer, burst_time):
-            print(str(a) + ' \t\t\t\t|', str(b) + ' \t\t|', str(d) + ' \t\t|', str(c))
+        for x, y, z, k in zip(process_names, burst_time, waiting_time, turn_around):
+            print(str(x) + ' \t\t\t\t|', str(y) + ' \t\t\t|', str(z) + ' \t\t\t|', str(k))
+        print('\n');
+        print('-' * 60)
+        print("Average Waiting Time : %s" % (sum(waiting_time) / len(waiting_time)))
+        print("Average Turn Around Time : %s" % (sum(turn_around) / len(turn_around)))
+        print('-' * 60)
 
 
 if __name__ == '__main__':
